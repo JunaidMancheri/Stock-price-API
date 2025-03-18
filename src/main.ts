6e8b5286cd9bfea86dd95cc9f9de8ea69c9ea900
Junaid <junaidofficialnow@gmail.com>
2025-03-18 09:21:27 +0530

    config: setup  routes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { Config } from './config';
 import { routes } from './routes';
+import createHttpError, { HttpError } from 'http-errors';
 
 const app = express();
 
 app.use('/api', routes);
+
+app.use('*', (req, res, next) => {
+    createHttpError(404, 'Not Found');
+})
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status).json({
+        error: true,
+        message: err.message || "Internal Server Error",
+        status: err.status || 500
+    })
+})
 
 app.listen(Config.port, (err) => {
     if (err) console.log(err);
--- src/routes/prices.routes.ts
+++ src/routes/prices.routes.ts
@@ -1,6 +1,9 @@
+import { getClosingPrice } from "@/prices/controller";
+import catchAsync from "@/utils/catchAsync";
 import { Router } from "express";
 
 const router = Router();
 
+router.get('/stock-price', catchAsync(getClosingPrice));
 
 export {router as pricesRouter};
